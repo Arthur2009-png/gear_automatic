81aa50295e502bc405b611734947f4677a1299d1
Arthur <jose.arthur.senai404@gmail.com>
2026-06-18 14:26:48 -0300

    novos carros

--- src/data/cars.js
+++ src/data/cars.js
@@ -10,10 +10,10 @@ import classicos from "../assets/classicos.png";
 
 
 export const CarsData = [
-  { id: 1, title: "Ford Mustang Shelby GT500", category: "Sport", banner: esportivo, fipe: 769000 },
+  { id: 1, title: "Ford Mustang Shelby", category: "Sport", banner: esportivo, fipe: 769000 },
   { id: 2, title: "Mclaren 720's", category: "Super", banner: supr, fipe: 2650000 },
-  { id: 3, title: "BMW4 Competition M xDrive", category: "Conversivel", banner: conversiveis, fipe: 415000 },
-  { id: 4, title: "Volvo XC60", category: "Suv", banner: suv, fipe: 168000 },
+  { id: 3, title: "BMW M4 Competition ", category: "Conversivel", banner: conversiveis, fipe: 550000 },
+  { id: 4, title: "Lamborghini Urus", category: "Suv", banner: suv, fipe: 3960000 },
   { id: 5, title: "BMW M2 xDrive", category: "Coup", banner: coup, fipe: 535000 },
   { id: 6, title: "Toyota Corolla XEI", category: "Sedans", banner: sedan, fipe: 151000 },
   { id: 7, title: "Fiat Touro", category: "Picapes", banner: picape, fipe: 315000 },
